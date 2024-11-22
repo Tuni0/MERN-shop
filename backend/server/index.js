eabf0ec9bb74159dbeb9ca84cd348fa087fbc75b
Tuni0 <wiktor.mazepa@bfsapp.pl>
2024-11-22 20:29:41 +0100

    first commit

---
+++ backend/server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+import helmet from "helmet";
+
+//config
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+//for images - to delete after
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+//MONGODB
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_CONNECT)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
