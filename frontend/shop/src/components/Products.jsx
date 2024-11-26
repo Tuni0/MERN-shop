7e85d62625576b80a98d711134a55732222c0562
Tuni0 <wiktor.mazepa@bfsapp.pl>
2024-11-26 22:39:49 +0100

    up

--- frontend/shop/src/components/Products.jsx
+++ frontend/shop/src/components/Products.jsx
@@ -182,13 +182,11 @@ const Products = () => {
                 <div className="relative group">
                   <a href={`products/${product.idproducts}`}>
                     <img
-                      key={product.idproducts}
                       alt={product.img_alt}
                       src={product.img_src}
                       loading="lazy"
                       className="z-0 relative aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
                     />
-                    s
                   </a>
                   <button
                     className="absolute z-20 top-2 right-16 flex items-center justify-center bg-white rounded-full p-1 shadow-md hover:bg-gray-200"
