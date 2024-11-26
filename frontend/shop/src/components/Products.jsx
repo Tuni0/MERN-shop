5a5b22bf5a8eadbb5612d4a5edc7a6bf24b997e1
Tuni0 <wiktor.mazepa@bfsapp.pl>
2024-11-26 22:37:31 +0100

    up

--- frontend/shop/src/components/Products.jsx
+++ frontend/shop/src/components/Products.jsx
@@ -188,6 +188,7 @@ const Products = () => {
                       loading="lazy"
                       className="z-0 relative aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-[7/8]"
                     />
+                    s
                   </a>
                   <button
                     className="absolute z-20 top-2 right-16 flex items-center justify-center bg-white rounded-full p-1 shadow-md hover:bg-gray-200"
